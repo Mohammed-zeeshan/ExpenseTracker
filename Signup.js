6a233be0a9354f63a966bcb3b29d35eded2fbe25
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-12-01 02:07:06 +0530

    Login Screen

--- Signup.js
+++ Signup.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import classes from './Signup.module.css';
 import AuthContext from '../pages/auth-context';
 
@@ -7,49 +7,59 @@ const Signup = () => {
     const emailInputRef = useRef();
     const passwordInputRef = useRef();
     const confirmPasswordInputRef = useRef();
+    const [isLogin, setIsLogin] = useState(true);
 
     const Authctx = useContext(AuthContext);
 
+    const switchAuthModeHandler = () => {
+        setIsLogin((prevState) => !prevState);
+      };
     const submitHandler = (event) => {
         event.preventDefault();
         const enteredEmail = emailInputRef.current.value;
         const enteredPassword = passwordInputRef.current.value;
         const enteredConfirmPassword = confirmPasswordInputRef.current.value;
         if (enteredPassword === enteredConfirmPassword) {
-            fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBSZZr78zGZyrw6I8PGUwpSUWOFci9rbm0',{
+            let url;
+            if (isLogin) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBSZZr78zGZyrw6I8PGUwpSUWOFci9rbm0'
+            } else {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBSZZr78zGZyrw6I8PGUwpSUWOFci9rbm0'
+            }
+            fetch(
+            url,{
                 method: 'POST',
                 body: JSON.stringify({
-                    email: enteredEmail,
-                    password: enteredPassword,
-                    returnSecureToken: true
+                email: enteredEmail,
+                password: enteredPassword,
+                returnSecureToken: true
                 }),
                 headers: {
-                    'Content-Type': 'application/json'
+                'Content-Type': 'application/json'
                 }
             }
             ).then(res => {
-                if (res.ok) {
-                    return res.json();
-                }
-                else {
-                    return res.json().then(() => {
-                        let errorMessage = 'Authentication failed';
-                        throw new Error(errorMessage);
-                    })
-                }
+            if (res.ok) {
+                return res.json();
+            } else {
+                return res.json().then((data) => {
+                let errorMessage = 'Authentication failed';
+                throw new Error(errorMessage);
+                });
+            }
             }).then(data => {
-                Authctx.login(data.idToken);
-              }).catch((err) => {
-                alert(err.message);
-              })
+            Authctx.login(data.idToken);
+            }).catch((err) => {
+            alert(err.message);
+            })
         }
         else {
             alert('Password does not match')
         }
     }
-  return (
+    return (
     <section className={classes.auth}>
-      <h1>SignUp</h1>
+      <h1>{isLogin ? 'Login' : 'SignUp'}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
@@ -64,7 +74,7 @@ const Signup = () => {
             ref={passwordInputRef}
           />
         </div>
-        <div className={classes.control}>
+        {!isLogin && <div className={classes.control}>
           <label htmlFor='password'>Confirm Password</label>
           <input
             type='password'
@@ -72,14 +82,15 @@ const Signup = () => {
             required
             ref={confirmPasswordInputRef}
           />
-        </div>
+        </div>}
         <div className={classes.actions}>
-          <button>Sign Up</button>
+        <button>{isLogin ? 'Login' : 'Sign Up'}</button>
           <button
             type='button'
             className={classes.toggle}
+            onClick={switchAuthModeHandler}
           >
-            Have an account? Login
+            {isLogin ? `Don't have account? Sign Up` : 'Have an account? Login'}
           </button>
         </div>
       </form>
