b59fc83cb3dd22c6379e86cc3d6371208b29a94c
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-12-01 23:16:42 +0530

    Logout User

--- Profile.js
+++ Profile.js
@@ -1,13 +1,28 @@
-import { useRef } from 'react'
+import { useContext, useState } from 'react'
 import classes from './Profile.module.css'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import AuthContext from '../pages/auth-context';
 
 const Profile = (props) => {
-    const nameInputRef = useRef();
-    const photoInputRef = useRef();
+    const [nameInput, setNameInput] = useState('');
+    const [photoInput, setPhotoInput] = useState('');
+    const authCtx = useContext(AuthContext)
+    const history = useHistory();
+    const logoutHandler = (event) => {
+        event.preventDefault();
+        history.replace('/');
+        authCtx.logout();
+    }
+    const nameInputHandler = (event) => {
+        setNameInput(event.target.value)
+    }
+    const photoInputHandler = (event) => {
+        setPhotoInput(event.target.value)
+    }
     const updateHandler = (event) => {
         event.preventDefault();
-        const name = nameInputRef.current.value;
-        const photourl = photoInputRef.current.value;
+        const name = nameInput;
+        const photourl = photoInput;
         let url = 'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBSZZr78zGZyrw6I8PGUwpSUWOFci9rbm0'
         fetch(
             url,{
@@ -41,13 +56,25 @@ const Profile = (props) => {
     const getData = () => {
         fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBSZZr78zGZyrw6I8PGUwpSUWOFci9rbm0',{
             method: 'POST',
-            body: JSON.stringify({idToken: props.id}),
+            body: JSON.stringify({idToken: props.id,returnSecureToken: true}),
             headers: {
                 'Content-Type': 'application/json'
             }
+        }).then(res => {
+            if(res.ok) {
+                return res.json();
+            }
+            else {
+                return res.json().then((data) => {
+                    let errorMessage = 'Authentication failed';
+                    throw new Error(errorMessage)
+                })
+            }
         })
         .then(data => {
-            console.log(data);
+            console.log(data.users[0]);
+            setNameInput(data.users[0].displayName);
+            setPhotoInput(data.users[0].photoUrl);
         })
         .catch(err => {
             console.log(err.message)
@@ -57,13 +84,14 @@ const Profile = (props) => {
     <div className={classes.container}>
         <div style={{flexDirection: 'row',display: 'flex', justifyContent: 'space-between'}}>
             <label>Contact Details</label>
-            <button className={classes.cancel}>Cancel</button>
+            <div><button className={classes.cancel}>Cancel</button>
+            <button onClick={logoutHandler}>Logout</button></div>
         </div>
         <div style={{padding:10}}>
             <label htmlFor='name'>Full Name: </label>
-            <input type='text' ref={nameInputRef} required/>
+            <input type='text' value={nameInput} onChange={nameInputHandler} required/>
             <label htmlFor='photo'>Profile Photo URL: </label>
-            <input type='text' ref={photoInputRef} required/>
+            <input type='text' value={photoInput} onChange={photoInputHandler} required/>
         </div>
         <button onClick={updateHandler}>Update</button>
     </div>
