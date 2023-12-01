4c305363921e66274bdc8298962eeb5b639c4a2e
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-12-01 17:27:45 +0530

    Edit User Details

--- Profile.js
+++ Profile.js
@@ -1,6 +1,58 @@
+import { useRef } from 'react'
 import classes from './Profile.module.css'
 
-const Profile = () => {
+const Profile = (props) => {
+    const nameInputRef = useRef();
+    const photoInputRef = useRef();
+    const updateHandler = (event) => {
+        event.preventDefault();
+        const name = nameInputRef.current.value;
+        const photourl = photoInputRef.current.value;
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBSZZr78zGZyrw6I8PGUwpSUWOFci9rbm0'
+        fetch(
+            url,{
+            method: 'POST',
+            body: JSON.stringify({
+                idToken: props.id,
+                displayName: name,
+                photoUrl: photourl,
+                returnSecureToken: true
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        ).then(res => {
+            if(res.ok) {
+                return res.json();
+            }
+            else {
+                return res.json().then((data) => {
+                    let errorMessage = 'Authentication failed';
+                    throw new Error(errorMessage)
+                })
+            }
+        }).then(data => {
+            console.log(data);
+        }).catch(err => {
+            console.log(err.message);
+        })
+    }
+    const getData = () => {
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBSZZr78zGZyrw6I8PGUwpSUWOFci9rbm0',{
+            method: 'POST',
+            body: JSON.stringify({idToken: props.id}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => {
+            console.log(err.message)
+        })
+    }
   return (
     <div className={classes.container}>
         <div style={{flexDirection: 'row',display: 'flex', justifyContent: 'space-between'}}>
@@ -8,12 +60,12 @@ const Profile = () => {
             <button className={classes.cancel}>Cancel</button>
         </div>
         <div style={{padding:10}}>
-            <label>Full Name: </label>
-            <input type='text' />
-            <label>Profile Photo URL: </label>
-            <input type='text' />
+            <label htmlFor='name'>Full Name: </label>
+            <input type='text' ref={nameInputRef} required/>
+            <label htmlFor='photo'>Profile Photo URL: </label>
+            <input type='text' ref={photoInputRef} required/>
         </div>
-        <button>Update</button>
+        <button onClick={updateHandler}>Update</button>
     </div>
   )
 }
