fb6ae211e65da35fedf4f7f298f73e478f137e96
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-12-02 17:44:15 +0530

    Integrating it with firebase

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -1,17 +1,55 @@
-import React, { useRef, useState } from 'react'
+import axios from 'axios';
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 const ExpenseForm = () => {
     const amountInputRef = useRef();
     const descriptionInputRef = useRef();
     const categoryInputRef = useRef();
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDescription, setEnteredDescription] = useState('');
-    const [enteredCategory, setEnteredCategory] = useState('');
-    const submitHandler = (event) => {
+    const [items, setItems] = useState([]);
+    const fetchItemsHandler = useCallback(async() => {
+        try {
+            const response = await fetch('https://track-expense-4f458-default-rtdb.firebaseio.com/Expenses.json')
+            if(!response.ok) {
+                throw new Error('Something went wrong');
+            }
+            const data = await response.json();
+            const loadedItems = [];
+            for (const key in data) {
+                loadedItems.push({
+                id: key,
+                amount: data[key].amount,
+                description: data[key].description,
+                category: data[key].category,
+                })
+            }
+            setItems(loadedItems);
+        }
+        catch (error) {
+            console.log(error.message);
+        }
+    }, [])
+    useEffect(() => {
+        fetchItemsHandler()
+    }, [fetchItemsHandler])
+    const submitHandler = async(event) => {
         event.preventDefault();
-        setEnteredAmount(amountInputRef.current.value);
-        setEnteredDescription(descriptionInputRef.current.value);
-        setEnteredCategory(categoryInputRef.current.value);
+        const data = {
+            amount: amountInputRef.current.value,
+            description: descriptionInputRef.current.value, 
+            category: categoryInputRef.current.value,
+        }
+        try {
+            const response = await axios.post('https://track-expense-4f458-default-rtdb.firebaseio.com/Expenses.json',data)
+            console.log(response.data);
+        }
+        catch (error) {
+            console.log(error.message)
+        }
+    }
+    let content = <p></p>;
+
+    if (items.length > 0){
+        <p>{items.amount} - {items.description} - {items.category}</p>
     }
   return (
     <section>
@@ -28,7 +66,7 @@ const ExpenseForm = () => {
                 </select>
             <button>Add</button>
         </form>
-        <p>{enteredAmount} {enteredDescription} {enteredCategory} </p>
+        {content}
     </section>
   )
 }
